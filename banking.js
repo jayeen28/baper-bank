6e230759a6a1c584d74abe130028e1cd936d3eac
jayeen29 <jayeen6783@gmail.com>
2021-08-17 01:11:58 +0600

    getTotalAmount function name changed and an extra step added to the function

--- banking.js
+++ banking.js
@@ -13,14 +13,19 @@ function getPreviousAmount(total) {
     const previousTotalNumber = parseFloat(previousTotalText);
     return previousTotalNumber;
 }
-
+function getAndSetTotalAmount(a, b, elementId) {
+    const result = a + b;
+    document.getElementById(elementId).innerText = result;
+    return result;
+}
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     //get the amount deposited
     const newDepositAmount = getInputValue('deposit-input');
     const previousDepositAmount = getPreviousAmount('deposit-total');
-    const totalDepositAmount = previousDepositAmount + newDepositAmount;
-    document.getElementById('deposit-total').innerText = totalDepositAmount;
+    getAndSetTotalAmount(newDepositAmount, previousDepositAmount, 'deposit-total');
+    //const totalDepositAmount = getAndSetTotalAmount(newDepositAmount, previousDepositAmount, 'deposit-total');
+    //document.getElementById('deposit-total').innerText = totalDepositAmount;
 
     //update account balance
     const balanceTotal = document.getElementById('balance-total');
@@ -34,8 +39,9 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     //get the withdraw amount
     const withdrawInputNum = getInputValue('withdraw-input');
     const withdrawTotalNum = getPreviousAmount('withdraw-total');
-    const newTotalWithdraw = withdrawInputNum + withdrawTotalNum;
-    document.getElementById('withdraw-total').innerText = newTotalWithdraw;
+    getAndSetTotalAmount(withdrawInputNum, withdrawTotalNum, 'withdraw-total');
+    //const newTotalWithdraw = getAndSetTotalAmount(withdrawInputNum, withdrawTotalNum, 'withdraw-total');
+    //document.getElementById('withdraw-total').innerText = newTotalWithdraw;
 
     //reduce balance
     const previousBalanceTotal = document.getElementById('balance-total');
