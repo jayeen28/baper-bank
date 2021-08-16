e03994a57a3dc333cb8e695548121e53723da557
jayeen29 <jayeen6783@gmail.com>
2021-08-17 01:58:01 +0600

    one function removed

--- banking.js
+++ banking.js
@@ -7,23 +7,17 @@ function getInputValue(inputId) {
     input.value = '';
     return inputNumber;
 }
-function getPreviousAmount(total) {
+function getPreviousAmount(total, inputAmount) {
     const previousTotal = document.getElementById(total);
     const previousTotalText = previousTotal.innerText;
     const previousTotalNumber = parseFloat(previousTotalText);
+    previousTotal.innerText = previousTotalNumber + inputAmount;
     return previousTotalNumber;
 }
-function getAndSetTotalAmount(a, b, elementId) {
-    const result = a + b;
-    document.getElementById(elementId).innerText = result;
-    return result;
-}
-
 document.getElementById('deposit-button').addEventListener('click', function () {
     //get the amount deposited
     const newDepositAmount = getInputValue('deposit-input');
-    const previousDepositAmount = getPreviousAmount('deposit-total');
-    getAndSetTotalAmount(newDepositAmount, previousDepositAmount, 'deposit-total');
+    getPreviousAmount('deposit-total', newDepositAmount);
 
     //update account balance
     const previousBalanceTotal = getPreviousAmount('balance-total');
@@ -34,8 +28,7 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 document.getElementById('withdraw-button').addEventListener('click', function () {
     //get the withdraw amount
     const withdrawInputNum = getInputValue('withdraw-input');
-    const withdrawTotalNum = getPreviousAmount('withdraw-total');
-    getAndSetTotalAmount(withdrawInputNum, withdrawTotalNum, 'withdraw-total');
+    getPreviousAmount('withdraw-total', withdrawInputNum);
 
     //reduce balance
     const balanceTotalNumber = getPreviousAmount('balance-total');
